e3613f2571b74e8cfd14827ae7f4571c74c41772
rivierea <alexis.riviere@outlook.fr>
2025-05-20 01:59:59 +0200

    allow front deployed

--- netflix-eco-back/Program.cs
+++ netflix-eco-back/Program.cs
@@ -25,7 +25,7 @@ builder.Services.AddCors(options =>
     options.AddPolicy("AllowLocalhost",
         policy =>
         {
-            policy.WithOrigins("http://localhost:3000")
+            policy.WithOrigins("http://localhost:3000","https://netflix-eco.onrender.com/")
                   .AllowAnyHeader()
                   .AllowAnyMethod();
         });
