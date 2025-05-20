d5b361fd2c1b8a99e9ee8030adfe851e68e22fbb
rivierea <alexis.riviere@outlook.fr>
2025-05-20 03:34:27 +0200

    fix last one

--- netflix-eco-back/Program.cs
+++ netflix-eco-back/Program.cs
@@ -25,7 +25,7 @@ builder.Services.AddCors(options =>
     options.AddPolicy("AllowLocalhost",
         policy =>
         {
-            policy.WithOrigins("http://localhost:3000","https://netflix-eco.onrender.com")
+            policy.WithOrigins("http://localhost:3000","https://netflix-eco-z8qw.onrender.com")
                   .AllowAnyHeader()
                   .AllowAnyMethod();
         });
